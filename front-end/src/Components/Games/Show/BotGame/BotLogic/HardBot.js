bf6cbeb342bcc2f178a19e0af78547a2e8ba42d2
gitlep1 <antonioshivers2500@yahoo.com>
2023-03-22 00:40:48 -0400

    working on bot logic

--- front-end/src/Components/Games/Show/BotGame/BotLogic/HardBot.js
+++ front-end/src/Components/Games/Show/BotGame/BotLogic/HardBot.js
@@ -1,19 +1,123 @@
-const MediumBot = (chessGame, game, safeGameMutate) => {
-  console.log("hard");
-  const possibleMoves = chessGame.moves();
+import { Chess } from "chess.js";
 
-  if (chessGame.game_over() || possibleMoves.length === 0) {
-    if (chessGame.turn() === "b") {
-      alert(`${game.player1} won`);
-    } else {
-      alert(`${game.player2} won`);
+const HardBot = (chessGame, setFen, depth) => {
+  console.log("inside hardbot func");
+
+  const makeMove = (game, move) => {
+    console.log("inside makeMove function");
+
+    game.move(move);
+    return game.fen();
+  };
+
+  const generateMoves = (game) => {
+    console.log("inside generateMoves function");
+
+    const moves = game.moves();
+    return moves.map((move) => makeMove(new Chess(game.fen()), move));
+  };
+
+  const evaluate = (fen) => {
+    console.log("evaluate function");
+
+    const game = new Chess(fen);
+    let score = 0;
+    const pieceValues = {
+      p: 1,
+      n: 3,
+      b: 3,
+      r: 5,
+      q: 9,
+      k: 0,
+    };
+
+    // Loop through all the pieces on the board
+    game.SQUARES.forEach((square) => {
+      const piece = game.get(square);
+
+      // If there is no piece on the square, move on to the next one
+      if (piece === null) {
+        return;
+      }
+
+      // Get the value of the piece based on its type
+      const value = pieceValues[piece.type];
+
+      // If the piece belongs to the AI player, add its value to the score
+      if (piece.color === game.turn()) {
+        score += value;
+      } else {
+        // If the piece belongs to the human player, subtract its value from the score
+        score -= value;
+      }
+    });
+
+    return score;
+  };
+
+  const generateTree = (fen, depth) => {
+    console.log("generating tree");
+
+    const game = new Chess(fen);
+    if (depth === 0 || game.game_over()) {
+      return { fen, score: evaluate(fen) };
     }
-  }
+    const moves = generateMoves(game);
+    const children = moves.map((fen) => generateTree(fen, depth - 1));
+    return {
+      fen,
+      children,
+      score:
+        depth % 2 === 0
+          ? Math.max(...children.map((child) => child.score))
+          : Math.min(...children.map((child) => child.score)),
+    };
+  };
 
-  const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-  safeGameMutate((chessGame) => {
-    chessGame.move(possibleMoves[randomIndex]);
-  });
+  const minimax = (tree, depth, isMaximizingPlayer) => {
+    console.log("minimax function");
+
+    if (depth === 0 || tree.children.length === 0) {
+      return tree.score;
+    }
+    if (isMaximizingPlayer) {
+      let bestScore = -Infinity;
+      for (const child of tree.children) {
+        const score = minimax(child, depth - 1, false);
+        bestScore = Math.max(bestScore, score);
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (const child of tree.children) {
+        const score = minimax(child, depth - 1, true);
+        bestScore = Math.min(bestScore, score);
+      }
+      return bestScore;
+    }
+  };
+
+  const chooseMove = (fen) => {
+    console.log("chooseMove function");
+
+    const tree = generateTree(fen, depth);
+    let bestScore = -Infinity;
+    let bestMove;
+    for (const child of tree.children) {
+      const score = minimax(child, depth - 1, false);
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = child.fen;
+      }
+    }
+    return bestMove;
+  };
+
+  const game = new Chess(chessGame);
+  const move = chooseMove(chessGame);
+  const newFen = makeMove(game, move);
+  console.log("new fen: ", newFen);
+  // setFen(newFen);
 };
 
-export default MediumBot;
+export default HardBot;
--- front-end/src/Components/Games/Show/BotGame/GamePageBot.js
+++ front-end/src/Components/Games/Show/BotGame/GamePageBot.js
@@ -3,252 +3,118 @@ import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
 import { Button, Modal, Image } from "react-bootstrap";
 
-import QueenImg from "../../../../Images/Queen.png";
-import RookImg from "../../../../Images/Rook.png";
-import BishopImg from "../../../../Images/Bishop.png";
-import KnightImg from "../../../../Images/Knight.png";
-
 import EasyBot from "./BotLogic/EasyBot";
 import MediumBot from "./BotLogic/MediumBot";
 import HardBot from "./BotLogic/HardBot";
 
-import { animalPieceTheme } from "../../../Accounts/Themes/Pieces/AnimalPieces";
-
 const PlayVsBot = ({ user, game, endGame }) => {
-  const chessboardRef = useRef();
+  const prevBoard = useRef([]);
+  const depth = 4;
+
   const [chessGame, setChessGame] = useState(new Chess());
   const [boardOrientation, setBoardOrientation] = useState("white");
-  const [currentTimeout, setCurrentTimeout] = useState(undefined);
+  const [fen, setFen] = useState(chessGame.fen());
+  const [promotionMove, setPromotionMove] = useState(null);
+  const [currentTimeout, setCurrentTimeout] = useState(null);
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [promoPiece, setPromoPiece] = useState("");
-  const [promoShow, setPromoShow] = useState(false);
-  const handlePromoClose = () => setPromoShow(false);
-  const handlePromoShow = () => setPromoShow(true);
+  useEffect(() => {
+    prevBoard.current.push(fen);
+  }, [fen]);
 
-  let player1 = game.player1id;
-  let player2 = game.player2id;
-
-  function safeGameMutate(modify) {
-    setChessGame((g) => {
-      const update = { ...g };
-      modify(update);
-      return update;
-    });
-  }
-
-  function makeRandomMove() {
+  const makeRandomMove = () => {
     if (game.player2id === 1) {
-      EasyBot(chessGame, game, safeGameMutate);
+      EasyBot(chessGame, game);
     } else if (game.player2id === 2) {
-      MediumBot(chessGame, game, safeGameMutate);
+      MediumBot(chessGame, game);
     } else if (game.player2id === 3) {
-      HardBot(chessGame, game, safeGameMutate);
+      HardBot(chessGame, setFen, depth);
     }
-  }
-
-  const onDrop = (sourceSquare, targetSquare, piece) => {
-    let promoArrWhite = ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"];
-    // use when allowing user to switch colors \\
-    // let promoArrBlack = ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"];
-
-    const gameCopy = { ...chessGame };
-
-    const move = gameCopy.move({
-      from: sourceSquare,
-      to: targetSquare,
-    });
-
-    if (move === null) {
-      return "snapback";
-    }
-
-    if (piece === "wP") {
-      for (const promote of promoArrWhite) {
-        if (targetSquare === promote) {
-          gameCopy.move({
-            from: sourceSquare,
-            to: targetSquare,
-            promotion: prompt("q, n, b, r"), // q, n, b, r
-          });
-          const newTimeout = setTimeout(makeRandomMove, 200);
-          setCurrentTimeout(newTimeout);
-          return true;
-        }
-      }
-    }
-
-    // illegal move
-    if (move === null) {
-      return "snapback";
-    }
-
-    setChessGame(gameCopy);
-
-    const newTimeout = setTimeout(makeRandomMove, 200);
-    setCurrentTimeout(newTimeout);
-    return true;
   };
 
-  const onDragStart = (source, piece, position, orientation) => {
-    if (chessGame.game_over()) {
-      return false;
+  const handleMove = (from, to, piece) => {
+    // Validate the move before making it
+    const move = chessGame.move({ from, to });
+    if (move) {
+      // Check for pawn promotion
+      if (move.flags.includes("p")) {
+        // Wait for promotion choice before making AI move
+        setPromotionMove({ from, to, piece });
+        return;
+      }
+
+      // Make the AI move after the user move
+      if (currentTimeout !== null) {
+        clearTimeout(currentTimeout);
+      }
+      const timeout = setTimeout(() => {
+        makeRandomMove();
+        setFen(chessGame.fen());
+        setCurrentTimeout(null);
+      }, 200);
+      setCurrentTimeout(timeout);
+    } else {
+      // Invalid move, reset the promotion move
+      setPromotionMove(null);
     }
-    if (chessGame.in_check()) {
-      console.log("check");
+  };
+
+  const handlePromotionChoice = (pieceType) => {
+    // Update the promotion move with the chosen piece
+    const newPiece = {
+      type: pieceType.toLowerCase(),
+      color: promotionMove.piece.color,
+    };
+    const newMove = chessGame.move({
+      from: promotionMove.from,
+      to: promotionMove.to,
+      promotion: newPiece.type,
+    });
+    chessGame.setPiece(newMove);
+
+    // Make the AI move after the user promotion
+    if (currentTimeout !== null) {
+      clearTimeout(currentTimeout);
     }
+    const timeout = setTimeout(() => {
+      makeRandomMove();
+      setFen(chessGame.fen());
+      setCurrentTimeout(null);
+    }, 200);
+    setCurrentTimeout(timeout);
+
+    // Reset the promotion move
+    setPromotionMove(null);
   };
 
   return (
     <section className="botBoard">
-      <div className="playerNames">
-        <span className="player2">
-          <img src={game.player2img} alt="player profile" />
-          <h3>{game.player2}</h3>
-        </span>
-        <p>VS</p>
-        <span className="player1">
-          <img src={game.player1img} alt="player profile" />
-          <h3>{game.player1}</h3>
-        </span>
-      </div>
       <div className="chessboard">
         <Chessboard
           id="PlayVsRandom" // use only if multiple boards
           boardOrientation={boardOrientation}
-          position={chessGame.fen()}
-          onPieceDrop={onDrop}
+          position={fen}
+          onPieceDrop={(from, to, piece) => handleMove(from, to, piece)}
           customBoardStyle={{
             borderRadius: "15%",
             boxShadow: "0 5px 15px rgba(0, 0, 0, 1)",
           }}
-          ref={chessboardRef}
-          onPieceDragBegin={onDragStart}
           areArrowsAllowed={false}
-          customPieces={animalPieceTheme}
+          // customPieces={animalPieceTheme}
+          animationDuration={500}
         />
+        {promotionMove && (
+          <div className="promotion-modal">
+            <button onClick={() => handlePromotionChoice("q")}>Queen</button>
+            <button onClick={() => handlePromotionChoice("r")}>Rook</button>
+            <button onClick={() => handlePromotionChoice("b")}>Bishop</button>
+            <button onClick={() => handlePromotionChoice("n")}>Knight</button>
+          </div>
+        )}
       </div>
-
-      {user.id === game.player1id ? (
-        <div className="botGameButtons">
-          <Button
-            variant="secondary"
-            className="rc-button"
-            onClick={() => {
-              safeGameMutate((chessGame) => {
-                chessGame.reset();
-              });
-              clearTimeout(currentTimeout);
-            }}
-          >
-            reset
-          </Button>
-          <Button
-            variant="dark"
-            className="rc-button"
-            onClick={() => {
-              setBoardOrientation((currentOrientation) =>
-                currentOrientation === "white" ? "black" : "white"
-              );
-            }}
-          >
-            flip board
-          </Button>
-          <Button
-            variant="primary"
-            className="rc-button"
-            onClick={() => {
-              safeGameMutate((chessGame) => {
-                chessGame.undo();
-                chessGame.undo();
-              });
-              clearTimeout(currentTimeout);
-            }}
-          >
-            undo
-          </Button>
-          <Button variant="danger" onClick={handleShow}>
-            END GAME
-          </Button>
-        </div>
-      ) : null}
-
-      <Modal show={show} onHide={handleClose} className="endgameModal">
-        <Modal.Header closeButton>
-          <Modal.Title>Do you want to end this game?</Modal.Title>
-        </Modal.Header>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button
-            variant="danger"
-            onClick={() => {
-              endGame(game.id);
-            }}
-          >
-            End Game
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
-      <Modal
-        show={promoShow}
-        onHide={handlePromoClose}
-        className="pawnPromotionModal"
-        centered
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header>
-          <Modal.Title>Pawn Promotion</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="pawnPromotionBody">
-          <Button
-            variant="secondary"
-            onClick={() => {
-              setPromoPiece("q");
-              handlePromoClose();
-            }}
-          >
-            <Image src={QueenImg} width={80} />
-            Queen
-          </Button>
-          <Button
-            variant="secondary"
-            onClick={() => {
-              setPromoPiece("r");
-              handlePromoClose();
-            }}
-          >
-            <Image src={RookImg} width={80} />
-            Rook
-          </Button>
-          <Button
-            variant="secondary"
-            onClick={() => {
-              setPromoPiece("b");
-              handlePromoClose();
-            }}
-          >
-            <Image src={BishopImg} width={80} />
-            Bishop
-          </Button>
-          <Button
-            variant="secondary"
-            onClick={() => {
-              setPromoPiece("n");
-              handlePromoClose();
-            }}
-          >
-            <Image src={KnightImg} width={80} />
-            Knight
-          </Button>
-        </Modal.Body>
-      </Modal>
     </section>
   );
 };
