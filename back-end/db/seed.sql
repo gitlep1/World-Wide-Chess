32e93bcebee90e4b13a461540907dd6af72dab08
gitlep1 <privserv4@gmail.com>
2023-10-20 17:19:28 -0400

    merging

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,9 +1,9 @@
 \c world_wide_chess;
 
 INSERT INTO users (profileimg, username, password, email, theme, chess_coins, wins, ties, loss, rating, preferred_color, is_guest, last_online) VALUES
-('https://www.randomlists.com/img/animals/tiger.webp', 'test1', 'test1', 'test1@test.com', 'default', 100, 3, 2, 1, 1050, 'w', false, NOW()),
-('https://brianrushwriter.files.wordpress.com/2014/11/27566315_s.jpg', 'test2', 'test', 'test2@test.com', 'light', 300, 3, 2, 5, 1000, 'b', false, NOW()),
-('https://i.natgeofe.com/k/02444b59-a50d-48e6-939b-4db10f895e66/5-reasons-eagle_4x3.jpg', 'test3', 'test3', 'test3@test.com', 'dark', 500, 5, 5, 5, 0, 'w', false, NOW());
+('https://www.randomlists.com/img/animals/tiger.webp', 'test1', 'test1', 'test1@test.com', 'default', 5000, 3, 2, 1, 1050, 'w', false, NOW()),
+('https://brianrushwriter.files.wordpress.com/2014/11/27566315_s.jpg', 'test2', 'test', 'test2@test.com', 'light', 3000, 3, 2, 5, 1000, 'b', false, NOW()),
+('https://i.natgeofe.com/k/02444b59-a50d-48e6-939b-4db10f895e66/5-reasons-eagle_4x3.jpg', 'test3', 'test3', 'test3@test.com', 'dark', 2000, 5, 5, 5, 0, 'w', false, NOW());
 
 INSERT INTO bots (profileimg, username, wins, ties, loss) VALUES 
 ('https://i.imgur.com/8puTSd9.png', 'Easy Bot', 10, 1, 1),
@@ -11,20 +11,20 @@ INSERT INTO bots (profileimg, username, wins, ties, loss) VALUES
 ('https://i.imgur.com/ZlrcYvd.png', 'Hard Bot', 100, 3, 3);
 
 INSERT INTO games (room_name, room_password, botId, player1id, player2id, player1color, player2color, botColor, current_positions, allow_specs, in_progress, is_multiplayer, game_time) VALUES
-('come watch', null, 1, '56fe4069-616e-4351-9a85-c720521dcfea', null, 'w', null, 'b', 'rnbqkbnr/pp1ppppp/8/2p5/8/5N2/PPPP1PPP/RNBQKB1R w KQkq - 0', false, true, false, 100000),
-('lol noobs', 'noobs', null, '12ab1853-81aa-4d05-9873-7b2a71f89755', null, 'b', null, null, 'rnbqkbnr/pp2pppp/3p4/8/3P4/8/PPP2PPP/RNBQKBNR w KQkq - 0', false, false, true, 200000),
-('easy game', 'hard game', 3, '1c7651a4-7f15-4a18-adf5-8de319a74940', null, 'w', null, 'b', '2kr3r/ppp1bppp/3q1n2/3p4/2PP4/2N2N2/PP2QPPP/R1B1K2R w KQ - 0', true, true, false, 300000),
-('the best', null, null, '56fe4069-616e-4351-9a85-c720521dcfea', '1c7651a4-7f15-4a18-adf5-8de319a74940', 'w', 'b', null, 'r1bqkbnr/ppp1pppp/2n5/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0', true, true, true, 400000);
+('come watch', null, 1, '883c83c0-78f8-4b2a-8325-b342184a1c8b', null, 'w', null, 'b', 'rnbqkbnr/pp1ppppp/8/2p5/8/5N2/PPPP1PPP/RNBQKB1R w KQkq - 0', false, true, false, 100000),
+('lol noobs', 'noobs', null, '97094bc3-36fd-4101-a7cf-400071c4babd', null, 'b', null, null, 'rnbqkbnr/pp2pppp/3p4/8/3P4/8/PPP2PPP/RNBQKBNR w KQkq - 0', false, false, true, 200000),
+('easy game', 'hard game', 3, '51631198-29f6-4ccb-86a4-7b09aa5a9909', null, 'w', null, 'b', '2kr3r/ppp1bppp/3q1n2/3p4/2PP4/2N2N2/PP2QPPP/R1B1K2R w KQ - 0', true, true, false, 300000),
+('the best', null, null, '883c83c0-78f8-4b2a-8325-b342184a1c8b', '51631198-29f6-4ccb-86a4-7b09aa5a9909', 'w', 'b', null, 'r1bqkbnr/ppp1pppp/2n5/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0', true, true, true, 400000);
 
 INSERT INTO spectators (spec_id, games_id) VALUES
-('56fe4069-616e-4351-9a85-c720521dcfea', 1),
-('12ab1853-81aa-4d05-9873-7b2a71f89755', 2),
-('1c7651a4-7f15-4a18-adf5-8de319a74940', 3);
+('883c83c0-78f8-4b2a-8325-b342184a1c8b', 1),
+('97094bc3-36fd-4101-a7cf-400071c4babd', 2),
+('51631198-29f6-4ccb-86a4-7b09aa5a9909', 3);
 
 INSERT INTO messages (user_id, username, profileimg, message) VALUES
-('56fe4069-616e-4351-9a85-c720521dcfea', 'test1', 'https://www.randomlists.com/img/animals/tiger.webp', 'hello'),
-('12ab1853-81aa-4d05-9873-7b2a71f89755', 'test2', 'https://brianrushwriter.files.wordpress.com/2014/11/27566315_s.jpg', 'noooobs'),
-('1c7651a4-7f15-4a18-adf5-8de319a74940', 'test3', 'https://i.natgeofe.com/k/02444b59-a50d-48e6-939b-4db10f895e66/5-reasons-eagle_4x3.jpg', 'lol');
+('883c83c0-78f8-4b2a-8325-b342184a1c8b', 'test1', 'https://www.randomlists.com/img/animals/tiger.webp', 'hello'),
+('97094bc3-36fd-4101-a7cf-400071c4babd', 'test2', 'https://brianrushwriter.files.wordpress.com/2014/11/27566315_s.jpg', 'noooobs'),
+('51631198-29f6-4ccb-86a4-7b09aa5a9909', 'test3', 'https://i.natgeofe.com/k/02444b59-a50d-48e6-939b-4db10f895e66/5-reasons-eagle_4x3.jpg', 'lol');
 
 INSERT INTO shop (item_img, item_name, item_price) VALUES
 ('https://picsum.photos/150/150', 'Dragon Border', 1000),
@@ -45,7 +45,7 @@ INSERT INTO shop (item_img, item_name, item_price) VALUES
 ('https://picsum.photos/165/165', 'Monochronic Chat Effects', 4000);
 
 INSERT INTO guest_inventory (guest_id, item_id, item_img, item_name) VALUES
-('12f6756d-50ae-4370-b06c-1757549f3469', 5, 'https://picsum.photos/154/154', 'Pawn Border'), ('12f6756d-50ae-4370-b06c-1757549f3469', 8, 'https://picsum.photos/157/157', 'Glass Piece Set'), ('12f6756d-50ae-4370-b06c-1757549f3469', 11, 'https://picsum.photos/160/160', 'Modern Board Style');
+('363460f3-0e9f-4c05-8424-0748393e61e5', 5, 'https://picsum.photos/154/154', 'Pawn Border'), ('363460f3-0e9f-4c05-8424-0748393e61e5', 8, 'https://picsum.photos/157/157', 'Glass Piece Set'), ('363460f3-0e9f-4c05-8424-0748393e61e5', 11, 'https://picsum.photos/160/160', 'Modern Board Style');
 
 INSERT INTO facts (fact_num, fact) VALUES
 (1, 'Legend has it that chess was invented around 200 B. C. by a commander, Hán Xin, who invented the game as a battle simulator. Soon after winning the battle, the game was forgotten, but it resurfaced in the 7th century.'),
