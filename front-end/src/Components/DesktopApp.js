f02cdb9d3797d033610e28b05c1a1e2ecca33610
gitlep1 <antonioshivers2500@yahoo.com>
2024-02-02 18:28:17 -0500

    trying to fix deploy

--- front-end/src/Components/DesktopApp.js
+++ front-end/src/Components/DesktopApp.js
@@ -12,7 +12,7 @@ import LeaderBoard from "./Leaderboard/LeaderBoard";
 import Shop from "./Shop/Shop";
 
 // Nav stuff \\
-import Navbar from "./Navbar/Navb";
+import Navbar from "./Navbar/Navbar";
 import Sidebar from "./Sidebar/Sidebar";
 import FoF from "./FourOFour/FoF";
 
