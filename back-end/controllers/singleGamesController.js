43903cd4b7ffdc9060217a8c3078b7caf45b502c
gitlep1 <antonioshivers2500@yahoo.com>
2024-06-03 00:01:01 -0400

    Updated: singleGamesController now ha try...catch in all requests

--- back-end/controllers/singleGamesController.js
+++ back-end/controllers/singleGamesController.js
@@ -21,13 +21,17 @@ const { requireAuth } = require("../validation/requireAuth");
 const { scopeAuth } = require("../validation/scopeAuth");
 
 games.get("/", requireAuth(), scopeAuth(["read:user"]), async (req, res) => {
-  const allGames = await getAllGames();
+  try {
+    const allGames = await getAllGames();
 
-  if (allGames) {
-    // console.log("=== GET games", allGames, "===");
-    res.status(200).json({ payload: allGames });
-  } else {
-    res.status(404).json({ error: "No chess games found." });
+    if (allGames) {
+      // console.log("=== GET games", allGames, "===");
+      res.status(200).json({ payload: allGames });
+    } else {
+      res.status(404).json({ error: "No chess games found." });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
@@ -36,14 +40,18 @@ games.get(
   requireAuth(),
   scopeAuth(["read:user", "write:user"]),
   async (req, res) => {
-    const id = _.escape(req.params.id);
-    const gotAGame = await getGameByID(id);
+    try {
+      const id = _.escape(req.params.id);
+      const gotAGame = await getGameByID(id);
 
-    if (gotAGame) {
-      // console.log("=== GET game", gotAGame, "===");
-      res.status(200).json({ payload: gotAGame });
-    } else {
-      res.status(404).json({ error: `Cannot get chess game with ID: ${id}` });
+      if (gotAGame) {
+        // console.log("=== GET game", gotAGame, "===");
+        res.status(200).json({ payload: gotAGame });
+      } else {
+        res.status(404).json({ error: `Cannot get chess game with ID: ${id}` });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
   }
 );
@@ -53,32 +61,36 @@ games.post(
   requireAuth(),
   scopeAuth(["read:user", "write:user"]),
   async (req, res) => {
-    const { token } = req.user;
-    const decoded = jwt.decode(token);
+    try {
+      const { token } = req.user;
+      const decoded = jwt.decode(token);
 
-    const checkUserData =
-      (await getUserByID(decoded.user.id)) ||
-      (await getGuestByID(decoded.user.id));
+      const checkUserData =
+        (await getUserByID(decoded.user.id)) ||
+        (await getGuestByID(decoded.user.id));
 
-    if (!checkUserData) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
+      if (!checkUserData) {
+        return res.status(401).json({ error: "Unauthorized" });
+      }
 
-    const newGameData = {
-      room_name: req.body.room_name,
-      room_password: req.body.room_password,
-      player1id: checkUserData.id,
-      allow_specs: req.body.allow_specs,
-      is_multiplayer: false,
-    };
+      const newGameData = {
+        room_name: req.body.room_name,
+        room_password: req.body.room_password,
+        player1id: checkUserData.id,
+        allow_specs: req.body.allow_specs,
+        is_multiplayer: false,
+      };
 
-    const newGame = await createGame(newGameData);
+      const newGame = await createGame(newGameData);
 
-    if (newGame) {
-      console.log("=== CREATE game", newGame, "===");
-      res.status(200).json({ payload: newGame });
-    } else {
-      res.status(404).json({ error: "Cannot create a new game." });
+      if (newGame) {
+        console.log("=== CREATE game", newGame, "===");
+        res.status(200).json({ payload: newGame });
+      } else {
+        res.status(404).json({ error: "Cannot create a new game." });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
   }
 );
@@ -88,43 +100,47 @@ games.put(
   requireAuth(),
   scopeAuth(["read:user", "write:user"]),
   async (req, res) => {
-    const id = _.escape(req.params.id);
+    try {
+      const id = _.escape(req.params.id);
 
-    const { token } = req.user;
-    const decoded = jwt.decode(token);
+      const { token } = req.user;
+      const decoded = jwt.decode(token);
 
-    const checkIfHost =
-      (await getUserByID(decoded.user.id)) ||
-      (await getGuestByID(decoded.user.id));
+      const checkIfHost =
+        (await getUserByID(decoded.user.id)) ||
+        (await getGuestByID(decoded.user.id));
 
-    const checkGameData = await getGameByID(id);
+      const checkGameData = await getGameByID(id);
 
-    // if (checkIfHost.id !== checkGameData.player1id) {
-    //   return res.status(401).json({ error: "Unauthorized" });
-    // } else {
-    const startingPositions =
-      "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+      // if (checkIfHost.id !== checkGameData.player1id) {
+      //   return res.status(401).json({ error: "Unauthorized" });
+      // } else {
+      const startingPositions =
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-    const updatedGameData = {
-      botId: req.body.botid,
-      player2id: req.body.player2id,
-      player1color: req.body.player1color,
-      player2color: req.body.player2color,
-      botColor: req.body.botcolor,
-      current_positions: startingPositions,
-      in_progress: true,
-      game_time: 0,
-    };
+      const updatedGameData = {
+        botId: req.body.botid,
+        player2id: req.body.player2id,
+        player1color: req.body.player1color,
+        player2color: req.body.player2color,
+        botColor: req.body.botcolor,
+        current_positions: startingPositions,
+        in_progress: true,
+        game_time: 0,
+      };
 
-    const updateGameData = await updateGame(id, updatedGameData);
-
-    if (updateGameData) {
-      console.log("=== UPDATE game", updateGameData, "===");
-      res.status(200).json({ payload: updateGameData });
-    } else {
-      res.status(404).json({ error: "Couldn't update game." });
+      const updateGameData = await updateGame(id, updatedGameData);
+      console.log({ updateGameData });
+      if (updateGameData) {
+        console.log("=== UPDATE game", updateGameData, "===");
+        res.status(200).json({ payload: updateGameData });
+      } else {
+        res.status(404).json({ error: "Couldn't update game." });
+      }
+      // }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
-    // }
   }
 );
 
@@ -133,64 +149,78 @@ games.put(
   requireAuth(),
   scopeAuth(["read:user", "write:user"]),
   async (req, res) => {
-    const id = _.escape(req.params.id);
-    const { token } = req.user;
-    const decoded = jwt.decode(token);
+    try {
+      const id = _.escape(req.params.id);
+      const { token } = req.user;
+      const decoded = jwt.decode(token);
 
-    const playerData =
-      (await getUserByID(decoded.user.id)) ||
-      (await getGuestByID(decoded.user.id));
+      const playerData =
+        (await getUserByID(decoded.user.id)) ||
+        (await getGuestByID(decoded.user.id));
 
-    const gameData = await getGameByID(id);
+      const gameData = await getGameByID(id);
 
-    const botData = await getBotById(gameData.botId);
+      const botData = await getBotById(gameData.botId);
 
-    if (
-      playerData.id !== gameData.player1id ||
-      playerData.id !== gameData.player2id ||
-      !botData.id
-    ) {
-      return res.status(401).json({ error: "Unauthorized" });
-    } else {
-      const { from, to, promotion } = req.body;
-
-      const chessGame = new Chess(gameData.current_positions);
-
-      if (promotion !== "") {
-        const move = chessGame.move({ from, to, promotion });
-        if (move) {
-          const updatedPositions = {
-            current_positions: move.after,
-          };
-
-          const updatePositions = await updateGamePositions(
-            id,
-            updatedPositions
-          );
-          console.log("updated game ID: ", id, "with data: ", updatePositions);
-          res.status(200).json({ payload: updatePositions });
-        } else {
-          console.log(`Could not update game: ${id}, INVALID MOVE!`);
-          res.status(404).json({ error: "Invalid promotion move" });
-        }
+      if (
+        playerData.id !== gameData.player1id ||
+        playerData.id !== gameData.player2id ||
+        !botData.id
+      ) {
+        return res.status(401).json({ error: "Unauthorized" });
       } else {
-        const move = chessGame.move({ from, to });
-        if (move) {
-          const updatedPositions = {
-            current_positions: move.after,
-          };
+        const { from, to, promotion } = req.body;
 
-          const updatePositions = await updateGamePositions(
-            id,
-            updatedPositions
-          );
-          console.log("updated game ID: ", id, "with data: ", updatePositions);
-          res.status(200).json({ payload: updatePositions });
+        const chessGame = new Chess(gameData.current_positions);
+
+        if (promotion !== "") {
+          const move = chessGame.move({ from, to, promotion });
+          if (move) {
+            const updatedPositions = {
+              current_positions: move.after,
+            };
+
+            const updatePositions = await updateGamePositions(
+              id,
+              updatedPositions
+            );
+            console.log(
+              "updated game ID: ",
+              id,
+              "with data: ",
+              updatePositions
+            );
+            res.status(200).json({ payload: updatePositions });
+          } else {
+            console.log(`Could not update game: ${id}, INVALID MOVE!`);
+            res.status(404).json({ error: "Invalid promotion move" });
+          }
         } else {
-          console.log(`Could not update game: ${id}, INVALID MOVE!`);
-          res.status(404).json({ error: "Invalid move" });
+          const move = chessGame.move({ from, to });
+          if (move) {
+            const updatedPositions = {
+              current_positions: move.after,
+            };
+
+            const updatePositions = await updateGamePositions(
+              id,
+              updatedPositions
+            );
+            console.log(
+              "updated game ID: ",
+              id,
+              "with data: ",
+              updatePositions
+            );
+            res.status(200).json({ payload: updatePositions });
+          } else {
+            console.log(`Could not update game: ${id}, INVALID MOVE!`);
+            res.status(404).json({ error: "Invalid move" });
+          }
         }
       }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
   }
 );
@@ -200,29 +230,33 @@ games.delete(
   requireAuth(),
   scopeAuth(["read:user", "write:user"]),
   async (req, res) => {
-    const id = _.escape(req.params.id);
-    const { token } = req.user;
-    const decoded = jwt.decode(token);
+    try {
+      const id = _.escape(req.params.id);
+      const { token } = req.user;
+      const decoded = jwt.decode(token);
 
-    const checkIfHost =
-      (await getUserByID(decoded.user.id)) ||
-      (await getGuestByID(decoded.user.id));
+      const checkIfHost =
+        (await getUserByID(decoded.user.id)) ||
+        (await getGuestByID(decoded.user.id));
 
-    const checkGameData = await getGameByID(id);
+      const checkGameData = await getGameByID(id);
 
-    if (checkIfHost.id !== checkGameData.player1id) {
-      return res.status(401).json({ error: "Unauthorized" });
-    } else {
-      const deletedGame = await deleteGame(id);
-
-      if (deletedGame.id) {
-        console.log("=== DELETE game", deletedGame, "===");
-        res.status(200).json({ payload: deletedGame });
+      if (checkIfHost.id !== checkGameData.player1id) {
+        return res.status(401).json({ error: "Unauthorized" });
       } else {
-        res
-          .status(404)
-          .json({ error: `Game with the ID: ${id} could not be deleted.` });
+        const deletedGame = await deleteGame(id);
+
+        if (deletedGame.id) {
+          console.log("=== DELETE game", deletedGame, "===");
+          res.status(200).json({ payload: deletedGame });
+        } else {
+          res
+            .status(404)
+            .json({ error: `Game with the ID: ${id} could not be deleted.` });
+        }
       }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
   }
 );
